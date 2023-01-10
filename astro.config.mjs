73ae653bd337ac13af994f225d811a567669992b
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2023-01-10 17:31:08 +0530

    Revert "Remove astro-compress"
    This reverts commit bbdde271413acfb118643335e6310717154cec67.

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,9 @@ import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
@@ -50,6 +53,7 @@ export default defineConfig({
     }),
     preact(),
     sitemap(),
+    compress(),
     robotsTxt(),
   ],
 });
